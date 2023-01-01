1a922207177e7c8a9a718bfd51c0aa629b531e40
ac125689 <atirumala844@gmail.com>
2023-01-01 14:10:49 -0500

    Update Newyear.py

--- Newyear.py
+++ Newyear.py
@@ -8,6 +8,14 @@ def main():
         st.balloons()
         st.snow()
         st.balloons()
+        st.header('This is the time to ....')
+        st.write('This is the time to be a new person')
+        st.write('This is the time to achive something new')
+        st.write('This is the time to make new memories')
+        st.write('This is the time to make new friends or strengthen old friendships')
+        st.write('This is the time to try something new')
+        st.write('And most of all ...')
+        st.write('This is the time of new beginnings')
 
 if __name__ == "__main__":
   main()
